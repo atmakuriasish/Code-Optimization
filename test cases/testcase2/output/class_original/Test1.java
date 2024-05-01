075021a7c4c937b290213e8dff4b3ea13e59f361
rohit <rsy708943@gmail.com>
2024-05-01 11:32:48 +0530

    openj9 and testcase

---
+++ test cases/testcase2/output/class_original/Test1.java
@@ -0,0 +1,27 @@
+class Test1{
+	public void bar(){
+		System.out.println("Test bar");
+	}
+	public static void main(String[] args) {
+		Test1 a,b;
+		for(int i=0;i<20000000;++i){
+			if(i%2==0){
+				a=new Test1();
+			}
+			else a=new Test2();
+			a.bar();
+			b=new Test1();
+			b.bar();
+		}
+	}
+}
+class Test2 extends Test1{
+	public void bar(){
+		System.out.println("Test2 bar");
+	}
+}
+class Test3 extends Test1{
+	public void bar(){
+		System.out.println("Test3 bar");
+	}
+}
